2a825cc69dd493ce14b6cb13d3ab43c62ceee0ef
Varun Reddy <136375960+varunx007@users.noreply.github.com>
2024-10-02 03:20:06 +0530

    Update slideSix.js

--- src/components/landing/slideSix.js
+++ src/components/landing/slideSix.js
@@ -14,11 +14,13 @@ const SlideSix = () => {
 
     return (
         <div className='bg-blue-100 dark:bg-black px-6 pb-16 pt-10'>
-            <div className="flex flex-col max-md:items-center justify-between border border-black bg-white py-10 px-16 dark:border-white dark:bg-black md:flex-row">
+            {/* Outer Background remains the same */}
+            <div className="flex flex-col max-md:items-center justify-between border border-black py-10 px-16 bg-[rgb(37,150,190)] dark:border-white md:flex-row">
+                {/* Inner section with updated background */}
                 <div className="mb-4 md:mb-0 md:mr-4 max-md:text-center">
                     <h2 className="mb-2 text-4xl max-md:text-2xl font-black dark:text-white">Get Started</h2>
                     <p className="flex items-center font-normal max-md:text-xs text-black dark:text-white">
-                        Embark on your learning journey with simplicity Get Started Today.
+                        Embark on your learning journey with simplicity. Get Started Today.
                     </p>
                 </div>
                 <div className="flex flex-shrink-0 items-center">
@@ -26,7 +28,6 @@ const SlideSix = () => {
                         onClick={redirectSignIn}
                         className="mr-3 inline-flex items-center justify-center border-black text-black border px-3 py-2 font-medium dark:border-white dark:text-white"
                     >
-
                         SignIn
                     </p>
                     <p
@@ -34,9 +35,7 @@ const SlideSix = () => {
                         className="mr-2 inline-flex items-center justify-center bg-black text-white px-3 py-2 font-medium dark:bg-white dark:text-black"
                     >
                         SignUp
-
                     </p>
-
                 </div>
             </div>
         </div>
@@ -44,4 +43,5 @@ const SlideSix = () => {
 };
 
 export default SlideSix;
+
 
