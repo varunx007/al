e4876eb1794f0c3271df6277888c4fc01fd97c62
Varun Reddy <136375960+varunx007@users.noreply.github.com>
2024-10-02 00:02:16 +0530

    Update slideTwo.js

--- src/components/landing/slideTwo.js
+++ src/components/landing/slideTwo.js
@@ -4,17 +4,17 @@ import { PiStudentFill, PiFeatherFill } from "react-icons/pi";
 
 const SlideTwo = () => {
     return (
-        <div className="px-7 justify-center items-center pb-10 dark:bg-black">
+        <div className="px-7 justify-center items-center bg-blue-100 dark:bg-black">
             <div className="flex flex-col md:flex-row items-center">
 
                 <div className="md:w-1/2 h-full p-4 flex flex-col items-center md:items-start justify-center">
 
-                    <h2 className="text-4xl font-black mb-2 max-md:text-2xl dark:text-white" >Unlock Endless Learning Opportunities</h2>
+                    <h2 className="text-4xl font-black mb-2 max-md:text-2xl dark:text-white" >Unlock Infinite Knowledge</h2>
 
 
                     <p className="text-black mb-2 mt-2 max-md:text-center max-md:text-xs dark:text-white">
-                    Effortlessly personalize your learning with our platform, enabling you to access engaging content on any topic.
-                    Seamlessly combine videos and informative content for a comprehensive learning experience, empowering effective education in just a few clicks.
+                        Craft compelling courses effortlessly with our platform, enabling you to create engaging content on any topic.
+                        Seamlessly integrate video and theory lectures for a comprehensive learning experience, fostering effective education in just a few clicks
                     </p>
 
 
@@ -31,8 +31,8 @@ const SlideTwo = () => {
                             <div className='max-md:flex max-md:justify-center max-md:items-center'>
                                 <PiFeatherFill className='text-2xl max-md:text-xl dark:text-white' />
                             </div>
-                            <h3 className="text-xl font-bold mb-2 max-md:text-xl dark:text-white">Learn with AI</h3>
-                            <p className='text-black max-md:text-xs dark:text-white'>Learn Any Topic with AI</p>
+                            <h3 className="text-xl font-bold mb-2 max-md:text-xl dark:text-white">Create Course</h3>
+                            <p className='text-black max-md:text-xs dark:text-white'>Create Course on Any Topic</p>
                         </div>
                     </div>
                 </div>
@@ -49,3 +49,4 @@ const SlideTwo = () => {
 };
 
 export default SlideTwo;
+
